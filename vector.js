690285bcb70513857c555f267e9bffb3a42664f6
FRACTAVA <mail@fractava.com>
2019-11-01 14:58:34 +0100

    Update vector.js

--- vector.js
+++ vector.js
@@ -1,33 +1,31 @@
-module.exports = {
-  class Vector{
-    constructor(x,y){
-      this.x= x;
-      this.y =y;
+export default class Vector{
+  constructor(x,y){
+    this.x= x;
+    this.y =y;
+  }
+  add(vector){
+    this.x += vector.x;
+    this.y += vector.y;
+  }
+  div(amount){
+    this.x /= amount;
+    this.y /= amount;
+  }
+  mult(amount){
+    this.x *= amount;
+    this.y *= amount;
+  }
+  mag(){
+     return Math.sqrt(this.x*this.x + this.y*this.y);
+  }
+  normalize(){
+    let m = this.mag();
+    if (m != 0 && m != 1) {
+      this.div(m);
     }
-    add(vector){
-      this.x += vector.x;
-      this.y += vector.y;
-    }
-    div(amount){
-      this.x /= amount;
-      this.y /= amount;
-    }
-    mult(amount){
-      this.x *= amount;
-      this.y *= amount;
-    }
-    mag(){
-       return Math.sqrt(this.x*this.x + this.y*this.y);
-    }
-    normalize(){
-      let m = this.mag();
-      if (m != 0 && m != 1) {
-        this.div(m);
-      }
-    }
-    setMag(amount){
-      this.normalize();
-      this.mult(amount);
-    }
+  }
+  setMag(amount){
+    this.normalize();
+    this.mult(amount);
   }
 }
