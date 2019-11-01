d770ba13620f667813ee05854226ad1af519de0c
fractava <57177630+fractava@users.noreply.github.com>
2019-11-01 13:35:06 +0100

    Create vector.js

---
+++ vector.js
@@ -0,0 +1,33 @@
+module.exports = {
+  class Vector{
+    constructor(x,y){
+      this.x= x;
+      this.y =y;
+    }
+    add(vector){
+      this.x += vector.x;
+      this.y += vector.y;
+    }
+    div(amount){
+      this.x /= amount;
+      this.y /= amount;
+    }
+    mult(amount){
+      this.x *= amount;
+      this.y *= amount;
+    }
+    mag(){
+       return Math.sqrt(this.x*this.x + this.y*this.y);
+    }
+    normalize(){
+      let m = this.mag();
+      if (m != 0 && m != 1) {
+        this.div(m);
+      }
+    }
+    setMag(amount){
+      this.normalize();
+      this.mult(amount);
+    }
+  }
+}
