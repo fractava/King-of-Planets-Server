2403e96dd315a1d26b79c7a0659f6acd2cdabf37
fractava <57177630+fractava@users.noreply.github.com>
2019-11-01 11:45:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const http = require('http')
 
 const port = 8080
 
+//Classes:
+class Map{
+  constructor(){
+    this.width = 0;
+    this.height = 0;
+    this.obstacles = [];
+  }
+  function collides(var x, var y, var w, var h){
+    
+  }
+  function loadJSON(var path){
+    
+  }
+}
+
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
@@ -11,3 +27,7 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running at port ${port}`)
 })
+
+setInterval(function(){
+  console.log("test");
+},33);
