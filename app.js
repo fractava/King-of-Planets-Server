33b0563e8a2ccc5593decdbe87b33141265fe910
fractava <57177630+fractava@users.noreply.github.com>
2019-11-01 13:37:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 const http = require('http')
 const fs = require('fs');
 
+require("vector.js");
+
+console.log(typeof Vector);
+
 const port = 8080;
 
 //Classes:
-class Vector{
-  constructor(x,y){
-    this.x= x;
-    this.y =y;
-  }
-  add(vector){
-    this.x += vector.x;
-    this.y += vector.y;
-  }
-  div(amount){
-    this.x /= amount;
-    this.y /= amount;
-  }
-  mult(amount){
-    this.x *= amount;
-    this.y *= amount;
-  }
-  mag(){
-     return Math.sqrt(this.x*this.x + this.y*this.y);
-  }
-  normalize(){
-    let m = this.mag();
-    if (m != 0 && m != 1) {
-      this.div(m);
-    }
-  }
-  setMag(amount){
-    this.normalize();
-    this.mult(amount);
-  }
-  
-}
 
 class Map{
   constructor(){
