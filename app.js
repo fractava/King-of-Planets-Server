fba1b1b79fbc7c1893ed0a30a9e6d050ab2e10de
fractava <57177630+fractava@users.noreply.github.com>
2019-11-01 13:27:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const fs = require('fs');
 const port = 8080;
 
 //Classes:
+class Vector{
+  constructor(x,y){
+    this.x= x;
+    this.y =y;
+  }
+  add(vector){
+    this.x += vector.x;
+    this.y += vector.y;
+  }
+  div(amount){
+    this.x /= amount;
+    this.y /= amount;
+  }
+  mult(amount){
+    this.x *= amount;
+    this.y *= amount;
+  }
+  mag(){
+     return Math.sqrt(this.x*this.x + this.y*this.y);
+  }
+  normalize(){
+    let m = this.mag();
+    if (m != 0 && m != 1) {
+      this.div(m);
+    }
+  }
+  setMag(amount){
+    this.normalize();
+    this.mult(amount);
+  }
+  
+}
+
 class Map{
   constructor(){
     this.obstacles = [];
@@ -32,6 +65,16 @@ class Obstacle{
    this.width = w;
    this.height = h;
  }
+}
+
+class Entity{}
+
+class AdmiralOculusShot extends Entity{
+  constructor(Owner,teamId,position){
+    this.speed = 8;
+    this.Owner = Owner;
+    this.teamId = teamId;
+  }
 }
 
 const server = http.createServer((req, res) => {
